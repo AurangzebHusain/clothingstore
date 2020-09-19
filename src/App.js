7e9ec38d0c72ef2e1cb9cae2e1cc387bf40c557f
unknown <aurangzebhusain786@gmail.com>
2020-09-19 17:56:59 +0530

    Homepage Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import Homepage from './components/Homepage.component';
+
+class App extends Component {
+  render() {
+    return <div className='App'>
+      <Homepage/>
+    </div>;
+  }
+}
+
+export default App;
